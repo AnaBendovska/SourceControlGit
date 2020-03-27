e08434a8f2810e684dd4c421e6d1e6519d7718e8
AnaBendovska <anna95bb@gmail.com>
2020-03-27 09:25:32 +0100

    Properties initialized in Calculator class.

--- SourceControlGit/Calculator.cs
+++ SourceControlGit/Calculator.cs
@@ -6,6 +6,16 @@ namespace SourceControlGit
 {
     public class Calculator
     {
+        public double num1 { get; set; }
+        public double num2 { get; set; }
 
+        public string typeOperation { get; set; }
+
+        public Calculator(double num1, double num2, string typeOperation)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+            this.typeOperation = typeOperation;
+        }
     }
 }
