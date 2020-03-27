a040dbe1988c0a5ef91811bc20a63f9e07005822
AnaBendovska <anna95bb@gmail.com>
2020-03-27 12:38:41 +0100

    Before checkout.

---
+++ SourceControlGit/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SourceControlGit
+{
+    class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Grade { get; set; }
+
+        public User(string name, string surname, string grade)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Grade = grade;
+        }
+
+        public void GetDetails()
+        {
+            Console.WriteLine("Name and Surname: {0} {1}, Grade: {2}", Name, Surname, Grade);
+        }
+    }
+}
